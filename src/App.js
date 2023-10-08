7be314b6635e0e6c821d796d213801ad2b6ef2f2
jhl8109 <jhl81094@gmail.com>
2023-10-09 02:27:45 +0900

    Feat : 거래 조정 페이지 생성

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import {Routes, Route} from 'react-router-dom';
 import { ThemeProvider,} from '@mui/styles';
 import { createTheme } from "@mui/material";
 import CarDetailComponent from './layouts/CarDetails/CarDetailComponent'
-import SellCar from './layouts/Transaction/SellCar'
+import SellCar from './layouts/CarSelling/SellCar'
 import SearchDashboard from './layouts/CarList/SearchDashboard';
+import CompromiseDashboard from 'layouts/CarCompromise/CompromiseDashboard';
 import Sidebar from "./components/Sidebar/Sidebar";
 import './app.css'
 import Profile from "./layouts/Profile/Profile"
@@ -26,6 +27,7 @@ const App = () => {
             <Route exact path="/home" element={< Album/>} />
             <Route exact path={"/buy"} element={<SearchDashboard />} />
             <Route exact path={"/sell"} element={<SellCar />} />
+            <Route exact path={"/compromise"} element={<CompromiseDashboard />} /> 
             <Route path="/buy/:id" element={< CarDetailComponent/>} />
             <Route exact path="/profile" element={< Profile/>} />
             <Route exact path="/signin" element={< SignIn/>} />
